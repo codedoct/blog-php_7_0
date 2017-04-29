3594de6c255e22c07a2004e9e8707032a4ee5d4e
victra <victra.sonata@gmail.com>
2017-04-30 00:28:16 +0700

    migration

--- database/migration.php
+++ database/migration.php
@@ -3,29 +3,29 @@
   $user="root";
   $pass="root";  
 
-  $dbname = "blog-php_7_0";
+  $dbname = "blog_php_7_0";
   $tablename = "crud";
   
   try {
-    $db = new PDO("mysql:host=$host", $user, $pass);
+    $db = new PDO("mysql:host=$host;CHARSET=utf8;COLLATE=utf8_unicode_ci", $user, $pass);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $db->exec("CREATE DATABASE IF NOT EXISTS `$dbname`; CREATE USER '$user'@'localhost' IDENTIFIED BY '$pass'; GRANT ALL ON `$dbname`.* TO '$user'@'localhost'; FLUSH PRIVILEGES;") 
+    $db->exec("CREATE DATABASE IF NOT EXISTS `$dbname`; CREATE USER '$user'@'localhost' IDENTIFIED BY '$pass'; GRANT ALL ON `$dbname`.* TO '$user'@'localhost'; FLUSH PRIVILEGES; DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci") 
     or die(print_r($db->errorInfo(), true));
   } catch(PDOException $e) {
     die("DB ERROR: ". $e->getMessage());
   }
 
   try {
-    $db = new PDO("mysql:dbname=$dbname;host=$host", $user, $pass);
+    $db = new PDO("mysql:dbname=$dbname;host=$host;CHARSET=utf8;COLLATE=utf8_unicode_ci", $user, $pass);
     $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     $db->exec("CREATE TABLE IF NOT EXISTS $tablename(
       id bigint( 20 ) AUTO_INCREMENT PRIMARY KEY,
-      name varchar( 50 ) NOT NULL,
-      email varchar( 150 ) NOT NULL, 
-      address text( 150 ) NOT NULL,
+      name varchar( 50 ) COLLATE utf8_unicode_ci NOT NULL,
+      email varchar( 150 ) COLLATE utf8_unicode_ci NOT NULL, 
+      address text( 150 ) COLLATE utf8_unicode_ci,
       created_at timestamp NULL,
       updated_at timestamp NULL
-    );");
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci");
     print("Created $tablename Table success.\n");
   } catch(PDOException $e) {
     echo $e->getMessage();
